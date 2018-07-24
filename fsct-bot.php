52f539b23321122587ee9528734a505ace93d335
wasan_jantawong <wasan.pipe142@gmail.com>
2018-07-24 15:17:33 +0700

    asfjofoof

--- fsct-bot.php
+++ fsct-bot.php
@@ -97,6 +97,7 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
-        curl_close ($ch);
     }
+
+curl_close ($ch);
 ?>
